f027e5dd37ceebee5300bc6416663c5196fd26ee
Maxim Suntsev <masuntsev@edu.hse.ru>
2020-05-27 02:24:02 +0500

    Added images, began to update the design, index page is now brand new.

--- src/main/java/com/shitajimado/academicwritingrecommender/controller/CorporaController.java
+++ src/main/java/com/shitajimado/academicwritingrecommender/controller/CorporaController.java
@@ -1,12 +1,20 @@
 package com.shitajimado.academicwritingrecommender.controller;
 
+import com.shitajimado.academicwritingrecommender.services.CorpusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CorporaController {
+    @Autowired
+    private CorpusService corpusService;
+
     @GetMapping(value = "/corpora")
-    public String corpora() {
-        return "corpora";
+    public ModelAndView corpora() {
+        var mv = new ModelAndView("corpora");
+        mv.addObject("corpora", corpusService.readCorpora());
+        return mv;
     }
 }
